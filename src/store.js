fa3e22873aa967a5c18144cc9437b4b4a06cb052
sdras <sarah.drasner@gmail.com>
2018-03-12 16:08:21 -0600

    add vuex

--- src/store.js
+++ src/store.js
@@ -5,12 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    counter: 0
   },
+  //showing things, not mutating state
+  getters: {
+    tripleCounter: state => {
+      return state.counter * 3
+    }
+  },
+  //mutating the state
+  //mutations are always synchronous
   mutations: {
-
+    //showing passed with payload, represented as num
+    increment: (state, num) => {
+      state.counter += num
+    }
   },
+  //commits the mutation, it's asynchronous
   actions: {
-
+    // showing passed with payload
+    incrementAsync({ commit }, num) {
+      setTimeout(() => {
+        commit('increment', num)
+      }, 1000)
+    }
   }
 })
